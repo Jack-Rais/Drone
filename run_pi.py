432569676f6d0737836bd8248c51dff28379e235
Jack-Rais <giorgio.caridi908@gmail.com>
2024-11-14 18:27:19 +0100

    None

--- run_pi.py
+++ run_pi.py
@@ -1,7 +1,8 @@
 # pip install nrf24, gigpio
 
 from nrf24 import NRF24, RF24_DATA_RATE, RF24_PA
-import pigpio                               
+import pigpio
+import struct
 import sys
 
 # You can get the HOST by running from the raspberry (sudo pigpoid) and then (hostname -I)
@@ -41,3 +42,32 @@ radio.set_retransmission(15, 15)
 radio.open_writing_pipe(ADRESS)
 
 radio.show_registers()
+
+while True:
+
+    print('Sent values')
+    try:
+
+        weather = 3.14              # Numero a caso
+        payload = struct.pack('<Bf', 0x01, weather)
+
+        print(payload)
+
+        radio.reset_packages_lost()
+        radio.send(payload)
+
+        try:
+            radio.wait_until_sent()
+        
+        except TimeoutError:
+            print('TimeoutError')
+
+        if radio.get_packages_lost() == 0:
+            print(f"Success: lost={radio.get_packages_lost()}, retries={radio.get_retries()}")
+        else:
+            print(f"Error: lost={radio.get_packages_lost()}, retries={radio.get_retries()}")
+
+    except:
+
+        radio.power_down()
+        pi.stop()
